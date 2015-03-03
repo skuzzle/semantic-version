724beffb6b23664ba31eb0b6547fb986ae764664
Simon Taddiken <simon.taddiken@gmail.com>
2015-03-03 10:09:02 +0100

    Remove deprecated method

--- src/main/java/de/skuzzle/Version.java
+++ src/main/java/de/skuzzle/Version.java
@@ -731,23 +731,6 @@ public final class Version implements Comparable<Version>, Serializable {
      * @param obj The object to compare with.
      * @return <code>true</code> iff {@code this.equals(obj)} and
      *         {@code this.getBuildMetaData().equals(((Version) obj).getBuildMetaData())}
-     * @since 0.2.0
-     * @deprecated Since 0.4.0 - will be removed in 0.5.0. Use
-     *             {@link #equalsWithBuildMetaData(Object)} instead.
-     */
-    @Deprecated
-    public boolean equalsIncludeBuildMetaData(Object obj) {
-        return testEquality(obj, true);
-    }
-
-    /**
-     * Determines whether this version is equal to the passed object (as
-     * determined by {@link #equals(Object)} and additionally considers the
-     * build meta data part of both versions for equality.
-     *
-     * @param obj The object to compare with.
-     * @return <code>true</code> iff {@code this.equals(obj)} and
-     *         {@code this.getBuildMetaData().equals(((Version) obj).getBuildMetaData())}
      * @since 0.4.0
      */
     public boolean equalsWithBuildMetaData(Object obj) {
