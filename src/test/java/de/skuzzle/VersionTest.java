988e505a09b973e5f02432f4cf98c08eb9177faa
Simon Taddiken <simon.taddiken@gmail.com>
2015-03-22 11:24:08 +0100

    Clean imports

--- src/test/java/de/skuzzle/VersionTest.java
+++ src/test/java/de/skuzzle/VersionTest.java
@@ -6,8 +6,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
